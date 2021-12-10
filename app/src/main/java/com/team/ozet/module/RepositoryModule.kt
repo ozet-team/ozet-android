dcc5bf14167e6902554618d1a89d133eeb89eeac
cloud0 <62924580+sen9040@users.noreply.github.com>
2021-12-11 00:49:34 +0900

    repository di 추가

--- app/src/main/java/com/team/ozet/module/RepositoryModule.kt
+++ app/src/main/java/com/team/ozet/module/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package com.team.ozet.module
 
+import com.ljjy.projecta.data.login.source.LoginRepository
+import com.ljjy.projecta.data.login.source.LoginRepositoryImpl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val repositoryModule: Module = module {
 //    single<HomeRepository> { HomeRepositoryImpl() }
-
+    single<LoginRepository> {LoginRepositoryImpl(get())}
 
 }
