548ad8aced72ebc3947adc178a0c1b1b6fde301f
sen9040 <62924580+sen9040@users.noreply.github.com>
2021-12-18 02:37:07 +0900

    passcode test 버전

---
+++ app/src/main/java/com/team/ozet/views/custom_view/DefaultAppbar.kt
@@ -0,0 +1,47 @@
+package com.team.ozet.views.custom_view
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.team.ozet.R
+import com.team.ozet.databinding.CustomAppbarBinding
+
+class DefaultAppbar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet?  = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var binding = DataBindingUtil.inflate<CustomAppbarBinding>(
+        LayoutInflater.from(context),R.layout.custom_appbar,this,true
+    )
+
+    init{
+        getAttrs(attrs, defStyleAttr)
+    }
+
+    private fun getAttrs(attrs: AttributeSet?,defStyleAttr: Int){
+        val typedArray:TypedArray = context.obtainStyledAttributes(attrs,R.styleable.default_appbar,defStyleAttr,0)
+        setTypeArray(typedArray)
+    }
+
+    private fun setTypeArray(typedArray: TypedArray) {
+        binding.apply {
+
+            imageView.setImageResource(
+                typedArray.getResourceId(R.styleable.default_appbar_srcCompat,-1)
+            )
+            imageView.setOnClickListener {
+            }
+            tvTitle.text = typedArray.getText(R.styleable.default_appbar_tv_appbar_title)
+
+            tvSubTitle.text = typedArray.getText(R.styleable.default_appbar_tv_appbar_sub_title)
+            tvSubTitle.setTextColor(typedArray.getColor(R.styleable.default_appbar_android_textColor, Color.BLACK))
+        }
+    }
+
+
+}
